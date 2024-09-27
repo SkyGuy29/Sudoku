9d5f3a4b343ee903b63750ee8fb53f72d6788371
SkyGuy29 <144573546+SkyGuy29@users.noreply.github.com>
2024-09-27 15:06:25 -0500

    backtracking solver is done and works garunteed.

--- sudoku.cpp
+++ sudoku.cpp
@@ -2,6 +2,11 @@
 Make a sudoku then solve it
 
 solver comes first, then the generator.
+
+sudoku rules:
+numbers placed may only go 1-9
+there cannot be two of the same number in a row, collumn, or 3x3 block
+the grid must be fully filled in and adhere to these other two rules.
 */
 
 #include <iostream>
@@ -15,15 +20,17 @@ void displayGrid(int[][9]);
 void displayCandidates(bool[][9][9], int, int);
 void displayRowCandidates(bool[][9][9], int);
 void displayColCandidates(bool[][9][9], int);
+bool backtrackSolve(int[][9], unsigned = 0);
+bool isSafe(int[][9], int, int, int);
+
 bool firstSolve(int[][9], bool[][9][9]);
 bool secondSolve(int[][9], bool[][9][9]);
-bool backtrackSolve(int[][9], int);
-bool isSafe(int[][9], int, int, int);
+
 
 int main()
 {
     //first two brackets are tied to a grid coordinate, last one is if candidate 1-9 is true
-    //if seven is a possible candidate of the middle grid space, candidates[4][4][6] would be true
+    //if seven is a possible candidate of the middle tile, candidates[4][4][6] would be true
     bool candidates[9][9][9]{}, updated = true;
     int choice, xCoord, yCoord, grid[9][9]
     {
@@ -36,8 +43,9 @@ int main()
         0, 0, 8, 7, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 8, 9, 0,
         5, 0, 0, 8, 9, 0, 0, 0, 0
-    }; //i literally got this from the new york times lol
+    };
 
+    //   readFile(grid); //WHY DOESNT PROGRAMIZ LET ME ADD A TEXT FILE RRRRRRRRRRRRGGGGGGGGHHHHHHHHH
     initializeCandidates(grid, candidates);
 
     //menu front end display
@@ -46,18 +54,17 @@ int main()
         if(updated)
             displayGrid(grid);
         else
-            std::cout << "No changes made!\n";
+            std::cout << "No changes made! \n";
         updated = true;
         std::cout << std::endl;
         std::cout 
-            << "1) end it all\n"
-            << "2) scream\n"
-            << "3) debug solver functions \n"
-            << "4) cout a value at a location\n" 
-            << "5) list candidates of a grid space \n"
+            << "0) exit \n"
+            << "1) scream \n"
+            << "2) set a tile's value \n"
+            << "5) list candidates of a tile \n"
             << "6) list row candidates \n"
             << "7) list collumn candidates \n"
-            << "8) backtrack solve \n"
+            << "8) solve \n"
             << std::endl
             << "pick an option: ";
         std::cin >> choice;
@@ -65,129 +72,198 @@ int main()
         //runs functions based on your option (listen im trying to comment everything that means this too)
         switch (choice)
         {
-            case 1:
+            case 0: //exit
                 return 0;
-            case 2:
+            case 1: //scream
                 while (true)
                     std::cout << 'A';
-            case 3:
-                std::cout << "pick a solve (1-2): ";
-                std::cin >> choice;
-                switch (choice)
-                {
-                    case 1:
-                        updated = firstSolve(grid, candidates);
-                        break;
-                    case 2:
-                        updated = secondSolve(grid, candidates);
-                }
-                break;
-            case 4: 
+            case 2: //set a tile's value
                 std::cout << "y and x coordinates: ";
                 std::cin >> yCoord >> xCoord;
-                std::cout << grid[yCoord][xCoord];
+                std::cout << "value: ";
+                std::cin >> choice;
+                if (grid[yCoord][xCoord] == 0)
+                {
+                    grid[yCoord][xCoord] = choice;
+                    updateCandidates(candidates, choice, yCoord, xCoord);
+                }
                 break;
-            case 5:
+            case 5: //list candidates of a tile
                 std::cout << "y and x coordinates: ";
                 std::cin >> yCoord >> xCoord;
                 displayCandidates(candidates, yCoord, xCoord);
                 break;
-            case 6:
+            case 6: //list candidates of a row
                 std::cout << "y coordinate: ";
                 std::cin >> yCoord;
                 displayRowCandidates(candidates, yCoord);
                 break;
-            case 7:
+            case 7: //list candidates of a collumn
                 std::cout << "x coordinate: ";
                 std::cin >> xCoord;
                 displayColCandidates(candidates, xCoord);
                 break;
-            case 8:
-                backtrackSolve(grid, 0);
+            case 8: //solve
+                backtrackSolve(grid);
         }
         std::cout << "done" << std::endl << std::endl;
         choice = 0;
     }
-    //solve using candidates
 
-    /*if two grid spaces in a row, collumn, or block share the same two candidates, the rest
-    of the grid spaces in the row, column, or block will not have those as candidates, and
-    we can remove them. may be smart to do this earlier than later.
-    this works for anything from 2 to 8 as well. replace the word "two" with the new
-    number and it will still hold true.
-    
-    doing this method */
-
-    //could this theoretically lead to an optimized version of this that involves recursively
-    //checking rows, collumns, and blockes after a change is made for any new possible values?
-    //that would be so sick if that worked :O
-    //I'm not really looking to code that rn tho lol
-
-    //AUTOMATICALLY IF THERE ARE NO CANDIDATES ON A BLANK SPACE SOMETHING WENT WRONG
-    //same thing goes for if there are no candidates or valued block for a value in a 
-    //row, collumn, or block
-    //...I've said that SOOO MUCH already good grief
-
-    //all of these different methods are going to need to be repeated several times until
-    //the puzzle is solved...
-
-    /*
-    brute force backtracking because i do not care
-
-    place down 1 on the first open grid space
-    check to see if it works
-    if it does, move to the next OPEN tile and repeat of that tile
-    if not, repeat, increasing the number by one
-    
-    if the number hits 9 and fails, clear the tile an move to the previous tile.
-    the previous tile will be incremented by one and checked once again
-
-    theoretically this should work
-    */
-
-
-
-    return 0;
+    //its a while(true) loop, the only exit is if you choose to exit.
 }
 
-bool backtrackSolve(int grid[][9], int coord)
+
+//loads a grid from the sudoku.txt file 
+void readFile(int grid[][9])
 {
-    /*
-    brute force backtracking because i do not care
-
-    place down 1 on the first open grid space
-    check to see if it works
-    if it does, move to the next OPEN tile and repeat of that tile
-    if not, repeat, increasing the number by one
-    
-    if the number hits 9 and fails, clear the tile an move to the previous tile.
-    the previous tile will be incremented by one and checked once again
-
-    theoretically this should work
-    */
-
-        if(grid[coord / 9][coord % 9] == 0)
-        {
-            for (int count = 1; count <= 9; ++count)
-            {
-                grid[coord / 9][coord % 9] = count;
-                std::cout << coord << std::endl;
-
-                if(isSafe(grid, coord / 9, coord % 9, count))
-                    if (coord >= 80 || backtrackSolve(grid, coord + 1))
-                        return true;
-            }
-        }
-        else
-            if (backtrackSolve(grid, coord + 1))
-                return true;
-
-
-    grid[coord / 9][coord % 9] = 0;
-    return false;
+    std::fstream file;
+    file.open("sudoku.txt", std::ios::in);
+    for (int i = 0; i < 81; ++i)
+        file >> grid[i / 9][i % 9];
+    file.close();
 }
 
-bool isSafe(int grid[][9], int y, int x, int value)
+
+//initializes possible candidates for values based on the values already in the grid
+void initializeCandidates(int grid[][9], bool candidates[][9][9])
+{
+    //method 1
+    //give every tile every candidate
+    for (int i = 0; i < 81; ++i)
+        if (grid[i / 9][i % 9] == 0)
+            for (int j = 0; j < 9; ++j)
+                candidates[i / 9][i % 9][j] = true;
+    
+    //if there is a value in a tile, run the update candidates function to remove
+    //any candidate that would relate to the value in the tile.
+    for (int i = 0; i < 81; ++i)
+        if (grid[i / 9][i % 9] != 0)
+            updateCandidates(candidates, grid[i / 9][i % 9] - 1, i / 9, i % 9);
+}
+
+
+//called after a tile is given a new value, updates candidates in the same row,
+//collumn, and block as the updated tile.
+//also called in initializing for the same reason.
+void updateCandidates(bool candidates[][9][9], int value, int y, int x)
+{
+    //after a tile is given a value, remove all of its candidates and remove
+    //all of the candidates that match that value in the row, collumn, AND block.
+    for (int i = 0; i < 9; ++i) //all tile candidates
+        candidates[y][x][i] = false;
+
+    for (int i = 0; i < 9; ++i) //row
+        candidates[y][i][value] = false;
+
+    for (int i = 0; i < 9; ++i) //collumn
+        candidates[i][x][value] = false;
+
+    for (int i = 0; i < 9; ++i) //block
+        candidates[y / 3 * 3 + i / 3][x / 3 * 3 + i % 3][value] = false;
+}
+
+
+//displays the grid to the console
+void displayGrid(int grid[][9])
+{
+    for (int i = 0; i < 81; ++i)
+    {
+        if (grid[i / 9][i % 9] == 0)
+            std::cout << ". ";
+        else
+            std::cout << grid[i / 9][i % 9] << ' ';
+        if ((i + 1) % 9 == 0)
+            std::cout << std::endl;
+    }
+}
+
+
+//displays the candidates of a specific tile
+void displayCandidates(bool candidates[][9][9], int y, int x)
+{
+    for (int i = 0; i < 9; ++i)   
+        if (candidates[y][x][i])
+            std::cout << i + 1 << ' ';
+    
+    std::cout << std::endl;
+}
+
+
+//displays the candidates of a whole row
+void displayRowCandidates(bool candidates[][9][9], int y)
+{
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            if(candidates[y][i][j])
+                std::cout << j + 1;
+            else
+                std::cout << ' ';
+
+            std::cout << ' ';
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+
+//displays the candidates of a whole collumn
+void displayColCandidates(bool candidates[][9][9], int x)
+{
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            if(candidates[i][x][j])
+                std::cout << j + 1;
+            else
+                std::cout << ' ';
+
+            std::cout << ' ';
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+
+//solves sudoku via a recursive backtracking algorithm. yep. brute force.
+//you CAN manually give a 
+bool backtrackSolve(int grid[][9], unsigned coord)
+{
+    if(grid[coord / 9][coord % 9] == 0) //if the tile is blank, we skip it. see the else statement below.
+    {
+        for (int count = 1; count <= 9; ++count) //repeat next steps for each value 1-9.
+        {
+            grid[coord / 9][coord % 9] = count; //first step. give tile a value.
+
+            if(isSafe(grid, count, coord / 9, coord % 9) //check to see if the value is good.
+                && (coord >= 80 || backtrackSolve(grid, coord + 1))) //backtrackSolve moves to the next tile.
+                //if the value is good and coord >= 80, the grid is solves, and this returns true.
+                //backtrackSolve will then chain return true to terminate the recursion.
+                return true;
+        }
+        
+        //if none of the nine values were successful, we reset the tile to blank
+        //and return false to recusrively move back to the previous tile.
+        grid[coord / 9][coord % 9] = 0;
+        return false;
+    }
+    else
+        //returns the same thing backtrackSolve does, unless coord >= 80, in which the grid is solved.
+        //calling backtrackSolve here effectively just skips over the filled in tile.
+        if (coord >= 80 || backtrackSolve(grid, coord + 1)) 
+            return true;
+        else
+            return false;
+}
+
+
+//checks to see if a value in a tile follows the sudoku rules.
+bool isSafe(int grid[][9], int value, int y, int x)
 {
     int count = 0;
 
@@ -220,121 +296,11 @@ bool isSafe(int grid[][9], int y, int x, int value)
 }
 
 
-//loads a grid from the sudoku.txt file 
-void readFile(int grid[][9])
-{
-    std::fstream file;
-}
+//the next two functions are obsolete, i put in so much work into them tho i dont want to delete them.
 
 
-//initializes possible candidates for values based on the values already in the grid
-void initializeCandidates(int grid[][9], bool candidates[][9][9])
-{
-    //method 1
-    //give every grid space every candidate
-    for (int i = 0; i < 81; ++i)
-        if (grid[i / 9][i % 9] == 0)
-            for (int j = 0; j < 9; ++j)
-                candidates[i / 9][i % 9][j] = true;
-    
-    //if there is a value in a grid space, run the update candidates function to remove
-    //any candidate that would relate to the value in the grid space.
-    for (int i = 0; i < 81; ++i)
-        if (grid[i / 9][i % 9] != 0)
-            updateCandidates(candidates, grid[i / 9][i % 9] - 1, i / 9, i % 9);
-}
-
-
-//called after a grid space is given a new value, updates candidates in the same row,
-//collumn, and block as the updated grid space.
-//also called in initializing for the same reason.
-void updateCandidates(bool candidates[][9][9], int value, int y, int x)
-{
-    //after a grid space is given a value, remove all of its candidates and remove
-    //all of the candidates that match that value in the row, collumn, AND block.
-    for (int i = 0; i < 9; ++i) //all grid space candidates
-        candidates[y][x][i] = false;
-
-    for (int i = 0; i < 9; ++i) //row
-        candidates[y][i][value] = false;
-
-    for (int i = 0; i < 9; ++i) //collumn
-        candidates[i][x][value] = false;
-
-    for (int i = 0; i < 9; ++i) //block
-        candidates[y / 3 * 3 + i / 3][x / 3 * 3 + i % 3][value] = false;
-}
-
-
-//displays the grid to the console
-void displayGrid(int grid[][9])
-{
-    for (int i = 0; i < 81; ++i)
-    {
-        if (grid[i / 9][i % 9] == 0)
-            std::cout << ". ";
-        else
-            std::cout << grid[i / 9][i % 9] << ' ';
-        if ((i + 1) % 9 == 0)
-            std::cout << std::endl;
-    }
-}
-
-
-//displays the candidates of a specific space
-void displayCandidates(bool candidates[][9][9], int y, int x)
-{
-    //to do: make a check for if the grid space has a value
-    //do this once you have checked that updateCandidates works properly
-    //the display should only list candidates of blank spaces
-    //theoretically, valued grid spaces should have no candidates
-    for (int i = 0; i < 9; ++i)   
-        if (candidates[y][x][i])
-            std::cout << i + 1 << ' ';
-    
-    std::cout << std::endl;
-}
-
-void displayRowCandidates(bool candidates[][9][9], int y)
-{
-    for (int i = 0; i < 9; ++i)
-    {
-        for (int j = 0; j < 9; ++j)
-        {
-            if(candidates[y][i][j])
-                std::cout << j + 1;
-            else
-                std::cout << ' ';
-
-            std::cout << ' ';
-        }
-
-        std::cout << std::endl;
-    }
-}
-
-
-void displayColCandidates(bool candidates[][9][9], int x)
-{
-    for (int i = 0; i < 9; ++i)
-    {
-        for (int j = 0; j < 9; ++j)
-        {
-            if(candidates[i][x][j])
-                std::cout << j + 1;
-            else
-                std::cout << ' ';
-
-            std::cout << ' ';
-        }
-
-        std::cout << std::endl;
-    }
-}
-
-
-//if there is only one candidate in the grid space it must be that one
-//returns true if a grid space was given a value, false if nothing happened.
+//if there is only one candidate in the tile it must be that one
+//returns true if a tile was given a value, false if nothing happened.
 bool firstSolve(int grid[][9], bool candidates[][9][9])
 {
     bool updated = false;
@@ -368,8 +334,8 @@ bool firstSolve(int grid[][9], bool candidates[][9][9])
 
 
 //if there is only one of a specific candidate in a row, collumn, or block
-//then that candidate must be the value in the grid space.
-//returns true if a grid space was given a value, false if nothing happened.
+//then that candidate must be the value in the tile.
+//returns true if a tile was given a value, false if nothing happened.
 //this doesnt seem optimized but im not sure how to fix it :/
 bool secondSolve(int grid[][9], bool candidates[][9][9])
 {
@@ -425,7 +391,7 @@ bool secondSolve(int grid[][9], bool candidates[][9][9])
         {
             //there is a reason for j / 3 * 3 + l / 3 and j % 3 * 3 + l % 3
             //j / 3 * 3 and j % 3 * 3 cycles through the blockes, and starts the search at the 
-            //top left grid space in each, and the l / 3 l % 3 cycles through the spaces in the block
+            //top left tile in each, and the l / 3 l % 3 cycles through the tiles in the block
             for (int l = 0; l < 9; ++l)
                 count += candidates[j / 3 * 3 + l / 3][j % 3 * 3 + l % 3][i];
 
@@ -449,4 +415,10 @@ bool secondSolve(int grid[][9], bool candidates[][9][9])
     }
 
     return updated;
+}
+
+void stackOverflow()
+{
+    int i;
+    stackOverflow();
 }
